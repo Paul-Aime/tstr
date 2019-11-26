5ed164c4b06f0b6fa1421d7fa4674338a818d558
Paul-Aime <paul.aime@grenoble-inp.org>
2019-11-26 20:09:19 +0100

    fix testReverbBloc.m overlap addition + add mona scripts

--- ressources/matlab/testReverbBloc.m
+++ ressources/matlab/testReverbBloc.m
@@ -30,29 +30,30 @@ imp_left = imp(:,1); % keep only one channel
 
 K = length(x);        % total length of the signal
 M = length(imp_left); % impusle response length
-L = 4100;             % lenght of the window (== BufferSize)
+L = 8192;             % lenght of the window (== BufferSize)
 K = K - mod(K,L);     % no simulation of non-full (last) buffer (underrun)
 
 
 %% Convolution in the temporal domain
 
-x_conv_tot = zeros(1, K+M-1);
-x_conv_prev = zeros(1, L+M-1);
-x_conv_next = zeros(1, L+M-1);
+x_conv_tot = zeros(K+M-1, 1);
+x_conv = zeros(L+M-1, 1);
 
 fprintf("Computing bloc-convolution...      ")
 step = 0;
 nb_step = K/L;
 for k = 1:L:K
-    % compute convolution for buffer k
-    x_conv_prev = myconv(x(k:k+L-1), imp_left); % size (L+M-1, 1)
-    
-    % save overlapping part in the next buffer
-    x_conv_next = zeros(L+M-1, 1);
-    x_conv_next(1:M-1) = x_conv_prev(L+1:end); % L+1 = end-(M-1)
+    % compute convolution for buffer k and add it to previous computation
+    x_conv = x_conv + myconv(x(k:k+L-1), imp_left); % size (L+M-1, 1)
     
     % output totally computed part (output buffer)
-    x_conv_tot(k:k+L-1) = x_conv_prev(1:L);
+    x_conv_tot(k:k+L-1) = x_conv(1:L);
+    
+    % save overlapping part in the next buffer
+    x_conv(1:M-1) = x_conv(L+1:end); % L+1 = end-(M-1)
+    
+    % reset the remaining buffer samples
+    x_conv(M:end) = 0;
     
     % disp progression
     step = step + 1;
@@ -62,7 +63,6 @@ fprintf('\n') % end of progression display
 
 % play result
 soundsc(x_conv_tot, Fs);
-% TODO there is some artefacts at each buffer borders
 
 
 %% Convolution in the frequential domain
