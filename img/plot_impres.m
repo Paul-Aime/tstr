b801775e4d9a2412b49c316c89215916c60db017
Paul-Aime <paul.aime@grenoble-inp.org>
2020-01-13 07:35:38 +0100

    Add plotting spectrum func

--- img/plot_impres.m
+++ img/plot_impres.m
@@ -15,15 +15,65 @@ grid on
 grid minor
 
 % Plot zoom #1
-range = 1:2048;
+range1 = 1:2048;
 axes('Position', [.35 .62 .5 .23])
-plot(impres(range))
-xlim([range(1)-1, range(end)])
+plot(impres(range1))
+xlim([range1(1)-1, range1(end)])
 grid on
 
 % Plot zoom #2
-range = 1:512;
+range2 = 1:512;
 axes('Position', [.35 .25 .5 .23])
-plot(impres(range))
-xlim([range(1)-1, range(end)])
+plot(impres(range2))
+xlim([range2(1)-1, range2(end)])
 grid on
+
+
+%% Spectrum
+
+% Time specifications
+fs = 44100;
+dt = 1/fs;
+nsamples = length(impres);
+t = (0:dt:(nsamples-1)*dt)';
+assert(size(t, 1) == nsamples);
+
+% Frequency specifications
+nfft = nsamples;
+df = fs/nfft;
+f = -fs/2:df:fs/2-df;
+
+% Fourier Transform
+S = fftshift(fft(impres(1:nsamples), nfft));
+S = abs(S) / nsamples; % normalize
+
+% Keep only >= 0 part 
+% TODO do it by going to hilbert representation of the signal
+S = S(f>=0);
+f = f(f>=0);
+
+% Sliding mean
+Sm = zeros(size(S)); % initialize
+ws_hz = 500; % window size in Hz
+ws = floor(ws_hz/df); % in number of samples
+if ~mod(ws, 2) % enforce odd value for ws
+    ws = ws - 1;
+end
+pl = S(1:floor((ws-1)/2)); % left padding
+pr = S(end-floor((ws-1)/2):end); % left padding
+Sp = [pl; S; pr]; % padding
+for idx = 1:length(S)
+    Sm(idx) = mean(Sp(idx:idx+ws-1));
+end
+
+% Plot the spectrum
+figure('Units', 'normalized', 'Position', [.2 .2 .7 .4])
+plot(f, S, 'b-');
+hold on
+hm = plot(f, Sm, 'g-', 'LineWidth', 2);
+xlim([0 f(end)])
+ax = gca;
+ax.XAxis.TickLabels = split(num2str(ax.XAxis.TickValues./1000)); % Hz -> kHz
+xlabel('Frequency [kHz]');
+title('Module of Fourier Transform')
+legend(hm, ['mean over ' num2str(ws_hz) ' Hz sliding window'])
